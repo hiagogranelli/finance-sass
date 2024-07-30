105e31b3544e91d565815fc4a175b858eb5bcdbe
Hiago Granelli <hiagogranelli12@gmail.com>
2024-07-29 22:21:04 -0300

    create sign-in and sign-up pages

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,22 @@
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1">
+      <div className="h-full lg: flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl">Bem-vindo de volta!</h1>
+        </div>
+        <div className="pt-16">
+          <ClerkLoading>
+            <Loader className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignUp />
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  );
+}
